67698b1a987cc2c0b7530db8eaa28f1bd7e3a0db
makhemiri <makhemiri@rs2i.fr>
2019-09-18 10:51:38 +0200

    select State

---
+++ src/app/prestations/components/item-prestation/item-prestation.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Prestation } from 'src/app/shared/models/prestation.model';
+import { State } from 'src/app/shared/enums/state.enum';
+
+@Component({
+  selector: 'app-item-prestation',
+  templateUrl: './item-prestation.component.html',
+  styleUrls: ['./item-prestation.component.scss']
+})
+export class ItemPrestationComponent implements OnInit {
+  @Input() item: Prestation;
+  // public states = Object.values(State); // transformer un objet en un tableau // if angular < 6
+  public states = State;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/prestations/prestations.module.ts
+++ src/app/prestations/prestations.module.ts
@@ -4,11 +4,12 @@ import { SharedModule } from '../shared/shared.module';
 import { ListPrestationsComponent } from './containers/list-prestations/list-prestations.component';
 import { PagePrestationsComponent } from './pages/page-prestations/page-prestations.component';
 import { PrestationsRoutingModule } from './prestations-routing.module';
+import { ItemPrestationComponent } from './components/item-prestation/item-prestation.component';
 
 
 
 @NgModule({
-  declarations: [PagePrestationsComponent, ListPrestationsComponent],
+  declarations: [PagePrestationsComponent, ListPrestationsComponent, ItemPrestationComponent],
   imports: [
     CommonModule,
     PrestationsRoutingModule,
