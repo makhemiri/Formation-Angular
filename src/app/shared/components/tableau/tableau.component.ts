e52abac35ffdda2e8a5e641084418ac3e5e4340e
makhemiri <makhemiri@rs2i.fr>
2019-09-18 15:03:45 +0200

    injection Template

---
+++ src/app/shared/components/tableau/tableau.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tableau',
+  templateUrl: './tableau.component.html',
+  styleUrls: ['./tableau.component.scss']
+})
+export class TableauComponent implements OnInit {
+  @Input() tableHeader: string[] ;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TableauComponent } from './components/tableau/tableau.component';
 import { StateDirective } from './directives/state.directive';
 import { TotalPipe } from './pipes/total.pipe';
 
 
 
 @NgModule({
-  declarations: [TotalPipe, StateDirective],
-  exports: [TotalPipe, StateDirective],
+  declarations: [TotalPipe, StateDirective, TableauComponent],
+  exports: [TotalPipe, StateDirective, TableauComponent],
   imports: [
     CommonModule
   ]
