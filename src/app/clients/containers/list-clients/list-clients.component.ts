95538cf05cdaaee42d4c11d5797c7690f2e1c39c
makhemiri <makhemiri@rs2i.fr>
2019-09-20 11:47:40 +0200

    + firebase pour client

--- src/app/clients/containers/list-clients/list-clients.component.ts
+++ src/app/clients/containers/list-clients/list-clients.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StateClient } from 'src/app/shared/enums/state-client.enum';
 import { Client } from 'src/app/shared/models/client.model';
 import { ClientsService } from '../../services/clients.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-clients',
@@ -9,18 +10,20 @@ import { ClientsService } from '../../services/clients.service';
   styleUrls: ['./list-clients.component.scss']
 })
 export class ListClientsComponent implements OnInit {
-  public collection: Client[];
+  public collection$: Observable<Client[]>;
   public tableHeader: string[] ;
   constructor(private clientsService: ClientsService) { }
 
   ngOnInit() {
-    this.collection = this.clientsService.collection;
+    this.collection$ = this.clientsService.collection;
     this.tableHeader = ['Client', 'Email', 'State'];
   }
 
   changeState(obj: {'item': Client , 'state': StateClient}) {
     // console.log('event', obj);
-    this.clientsService.update(obj.item, obj.state);
+    this.clientsService.update(obj.item, obj.state).then(() => {
+      obj.item.state = obj.state ;
+    });
 
   }
 }
--- src/app/clients/services/clients.service.ts
+++ src/app/clients/services/clients.service.ts
@@ -2,25 +2,49 @@ import { Injectable } from '@angular/core';
 import { StateClient } from 'src/app/shared/enums/state-client.enum';
 import { Client } from 'src/app/shared/models/client.model';
 import { fakeClients } from './fake-clients';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
-  private pCollection: Client[] ;
-  constructor() {
-    this.collection = fakeClients;
-    }
+  private pCollection: Observable<Client[]>;
+  private itemsCollection: AngularFirestoreCollection<Client>;
+
+
+
+
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Client>('clients');
+    this.collection = this.itemsCollection.valueChanges().pipe(
+      // map(tab => tab.map(obj => new Prestation(obj));  ====>  lambda  expression  ES6
+      map((tab) => {
+        return tab.map((obj) => {
+          return new Client(obj);
+        });
+      })
+    );
+    //this.collection = fakeClients;
+  }
+
 
   // get collection
-get collection(): Client[] {
-  return this.pCollection ;
-}
+  get collection(): Observable<Client[]> {
+    return this.pCollection;
+  }
   // set collection
-set collection(col: Client[]) {
-  this.pCollection = col;
-}
-public update(item: Client , state: StateClient) {
-  item.state = state;
-}
+  set collection(col: Observable<Client[]>) {
+    this.pCollection = col;
+  }
+  public update(item: Client, state?: StateClient): Promise<any> {
+    const client  = {...item};
+    if (state) {
+      client.state = state;
+    }
+    return this.itemsCollection.doc(item.id).update(client).catch((e) => {
+      console.log(e);
+    });
+  }
 }
